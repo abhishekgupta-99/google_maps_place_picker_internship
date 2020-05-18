feb2412ac363ce5e2bf318076aa8ae4a0914f2c5
abhishekgupta97023 <abhishekgupta97023@gmail.com>
2020-05-18 18:43:38 +0530

    Initial commit

---
+++ example/lib/credentials.dart
@@ -0,0 +1,10 @@
+//const PLACES_API_KEY="AIzaSyAYkq3N1PBHQn99yq5Xzf4rzHLobJutcT0";
+
+const search_query="Bhavan College, Andheri west, Mumbai";
+
+
+//mapsmedium
+const PLACES_API="AIzaSyDEx5cJ4-BmBTW1Ebs_BhyvO0zh5vGiyDg";
+
+//omkar sir api key
+//const PLACES_API_KEY="AIzaSyD0CxI7Ehu0Qpk0us4R5Gclb9xBCs6AeKM";
